28973319985894d799c9cf3fef56e018ec42de57
dudow <edu30.nm@gmail.com>
2025-02-18 12:18:11 -0300

    feat: add country interface

--- src/common/services/Countries.tsx
+++ src/common/services/Countries.tsx
@@ -1,7 +1,8 @@
+import { Country } from "../interfaces/Country";
 import { radioBrowserApi } from "./radioBrowserApi";
 
 const getCountries = async () => {
-  const response = await radioBrowserApi.get("countries");
+  const response = await radioBrowserApi.get<Country>("countries");
   return response.data;
 };
 
