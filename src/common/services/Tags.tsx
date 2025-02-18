c778fdb403fb03080d037a59d9b4febe3cdcc09a
dudow <edu30.nm@gmail.com>
2025-02-18 12:14:42 -0300

    feat: create tag interface

--- src/common/services/Tags.tsx
+++ src/common/services/Tags.tsx
@@ -1,7 +1,8 @@
+import { Tag } from "../interfaces/Tag";
 import { radioBrowserApi } from "./radioBrowserApi";
 
 const getTags = async () => {
-  const response = await radioBrowserApi.get("tags");
+  const response = await radioBrowserApi.get<Tag[]>("tags");
   return response.data;
 };
 
