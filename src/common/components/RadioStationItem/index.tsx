f76f0e55cad9fb090d33b228895bff6933ef9490
dudow <edu30.nm@gmail.com>
2025-02-19 13:26:26 -0300

    feat: create RadioStationItem component

---
+++ src/common/components/RadioStationItem/index.tsx
@@ -0,0 +1,13 @@
+const RadioStationItem = () => {
+  return (
+    <div className="flex gap-3 p-2 hover:bg-gray-800 rounded-lg">
+      <div className="w-12 h-12 bg-green-400 rounded"></div>
+      <div className="flex flex-col justify-evenly">
+        <p className="font-semibold">Liked Songs</p>
+        <p className="text-sm">Playlist - 311 songs</p>
+      </div>
+    </div>
+  );
+};
+
+export default RadioStationItem;
