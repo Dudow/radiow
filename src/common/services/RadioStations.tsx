e03f523ce0c08f4c15f695d5552296589fe78a75
dudow <edu30.nm@gmail.com>
2025-02-20 11:20:39 -0300

    feat: add params for search

--- src/common/services/RadioStations.tsx
+++ src/common/services/RadioStations.tsx
@@ -1,16 +1,19 @@
 import { RadioStation } from "../interfaces/RadioStation";
 import { radioBrowserApi } from "./radioBrowserApi";
 
-const getRadioStations = async () => {
-  const response = await radioBrowserApi.get<RadioStation[]>("stations", {
-    params: {
-      limit: 10,
-      order: "votes",
-      reverse: "true",
-      hidebroken: "true",
-    },
-  });
+export const getRadioStations = async (name?: string) => {
+  const response = await radioBrowserApi.get<RadioStation[]>(
+    "stations/search",
+    {
+      params: {
+        limit: 10,
+        order: "votes",
+        reverse: "true",
+        hidebroken: "true",
+        offset: 0,
+        name: name || "",
+      },
+    }
+  );
   return response.data;
 };
-
-export default getRadioStations;
