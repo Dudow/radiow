5a7a8fc32d26a91ff5027fbbb933931b798ec0f8
dudow <edu30.nm@gmail.com>
2025-02-20 15:13:16 -0300

    feat: add radio station item component

--- src/common/components/RadioStationItem/index.tsx
+++ src/common/components/RadioStationItem/index.tsx
@@ -1,9 +1,15 @@
-const RadioStationItem = () => {
+import { RadioStation } from "../../interfaces/RadioStation";
+
+interface RadioStationItemProps {
+  station: RadioStation;
+}
+
+const RadioStationItem = ({ station }: RadioStationItemProps) => {
   return (
     <div className="flex gap-3 p-2 hover:bg-gray-800 rounded-lg">
       <div className="w-12 h-12 bg-green-400 rounded"></div>
       <div className="flex flex-col justify-evenly">
-        <p className="font-semibold">Liked Songs</p>
+        <p className="font-semibold">{station.name}</p>
         <p className="text-sm">Playlist - 311 songs</p>
       </div>
     </div>
