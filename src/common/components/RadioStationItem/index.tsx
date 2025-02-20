d22e89c3a976a7ebcd7f79a323686293aecf989e
dudow <edu30.nm@gmail.com>
2025-02-20 15:42:47 -0300

    feat: make RadioStationItem usable and style it

--- src/common/components/RadioStationItem/index.tsx
+++ src/common/components/RadioStationItem/index.tsx
@@ -1,16 +1,35 @@
 import { RadioStation } from "../../interfaces/RadioStation";
+import playIcon from "../../assets/play-solid.svg";
 
 interface RadioStationItemProps {
   station: RadioStation;
+  index: number;
 }
 
-const RadioStationItem = ({ station }: RadioStationItemProps) => {
+const RadioStationItem = ({ station, index }: RadioStationItemProps) => {
+  const { name, votes, country, favicon } = station;
+
   return (
-    <div className="flex gap-3 p-2 hover:bg-gray-800 rounded-lg">
-      <div className="w-12 h-12 bg-green-400 rounded"></div>
+    <div className="flex gap-3 p-2 hover:bg-gray-800 rounded-lg group">
+      <div className="w-5 flex justify-center items-center relative">
+        <p className="inline group-hover:hidden text-white">{index + 1}</p>
+
+        <img
+          src={playIcon}
+          alt=""
+          className="invert absolute w-3 hidden group-hover:inline"
+        />
+      </div>
+      <div className="w-12 h-12 bg-gradient-to-br from-green-900 to-green-600  rounded p-1 flex justify-center items-center ">
+        {favicon && (
+          <img src={favicon} alt="station favicon" className="w-full" />
+        )}
+      </div>
       <div className="flex flex-col justify-evenly">
-        <p className="font-semibold">{station.name}</p>
-        <p className="text-sm">Playlist - 311 songs</p>
+        <p className="font-semibold">{name}</p>
+        <p className="text-sm">
+          {country ? country + " • " : ""} {votes} votes
+        </p>
       </div>
     </div>
   );
