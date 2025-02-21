6efb9da33806ad617cdf15c0721f6a41224e8781
dudow <edu30.nm@gmail.com>
2025-02-20 22:55:22 -0300

    fix: avoid breaking app

--- src/common/hooks/useLocalStorage.ts
+++ src/common/hooks/useLocalStorage.ts
@@ -1,13 +1,24 @@
+import { useCallback, useEffect } from "react";
 import { RadioStation } from "../interfaces/RadioStation";
+import { useFavoriteContext } from "./useFavoriteContext";
 
 export const useLocalStorage = () => {
+  const { setFavoriteStations } = useFavoriteContext();
+
   const saveFavorites = (stations: RadioStation[]) => {
     localStorage.setItem("favorites", JSON.stringify(stations));
   };
 
-  const getFavorites = () => {
+  const getFavorites = useCallback(() => {
     return localStorage.getItem("favorites");
-  };
+  }, []);
+
+  useEffect(() => {
+    const hasFavorites = getFavorites();
+
+    if (hasFavorites && hasFavorites.length > 2)
+      setFavoriteStations(JSON.parse(hasFavorites));
+  }, [getFavorites, setFavoriteStations]);
 
   return {
     saveFavorites,
