db48d6021400a7d6a91c3a2a9a90bc9e7654c5f2
dudow <edu30.nm@gmail.com>
2025-02-21 16:36:17 -0300

    fix: refactor radioStationItem

--- src/common/components/RadioStationItem/use.ts
+++ src/common/components/RadioStationItem/use.ts
@@ -1,1 +1,45 @@
-export const useRadioStationItem = () => {};
+import { useFavoriteContext } from "../../contexts/FavoritesContext/FavoritesContext";
+import { useSelectedSongContext } from "../../contexts/SelectedSongContext/SelectedSongContext";
+import { useLocalStorage } from "../../hooks/useLocalStorage";
+import { RadioStation } from "../../interfaces/RadioStation";
+
+export const useRadioStationItem = (station: RadioStation) => {
+  const { setFavoriteStations, filteredFavoriteStations, favoriteStations } =
+    useFavoriteContext();
+  const { saveFavorites } = useLocalStorage();
+  const { selectedStation, setSelectedstation } = useSelectedSongContext();
+
+  const alreadyAdded = filteredFavoriteStations.find(
+    (favoritedStation) => favoritedStation.stationuuid === station.stationuuid
+  );
+  const isSelectedStation = selectedStation.stationuuid === station.stationuuid;
+
+  const handleFavorite = () => {
+    if (alreadyAdded) {
+      const newFavorites = filteredFavoriteStations.filter(
+        (favoritedStation) =>
+          favoritedStation.stationuuid !== alreadyAdded.stationuuid
+      );
+      saveFavorites(newFavorites);
+      return setFavoriteStations(newFavorites);
+    }
+
+    if (favoriteStations.length === 10) return;
+
+    setFavoriteStations((prev) => {
+      const newFavoriteStations = [...prev, station];
+
+      saveFavorites(newFavoriteStations);
+
+      return newFavoriteStations;
+    });
+  };
+
+  return {
+    selectedStation,
+    setSelectedstation,
+    isSelectedStation,
+    handleFavorite,
+    alreadyAdded,
+  };
+};
